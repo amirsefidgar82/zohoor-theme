fcb36ec39c54cc1b4291407611d50a69a3b219f9
amirali <amirali.programer2017@gmail.com>
2022-11-25 19:31:08 +0330

    add coment in project and cleen them

--- assets/js/modal-sign_up.js
+++ assets/js/modal-sign_up.js
@@ -1,3 +1,5 @@
+// send phone number to jango 09333333333 
+
 function send_ponenumber() {
     $.ajax({
         type: 'POST',
@@ -15,12 +17,13 @@ function send_ponenumber() {
         }
     });
 
-
-
+    // add phonenumber to next modal to title phone number
     document.getElementById("form-code-titel-phone_number").innerText = $('#validation-phonenumber').val();
+
+    // countdown time 
     countdown_timer();
 }
-
+// function countdown timer and you can checne his count down  countdown(minutes, seconds);
 function countdown_timer() {
     function countdown(minutes, seconds) {
         function tick() {
@@ -33,27 +36,26 @@ function countdown_timer() {
                 timeoutHandle = setTimeout(tick, 1000);
             } else {
                 if (minutes >= 1) {
-                    // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
                     setTimeout(function() {
                         countdown(minutes - 1, 59);
                     }, 1000);
 
                 }
 
-            }
+            } // if seconds come to 0 chenge inner
             if (seconds < "0" && minutes == "0") {
                 document.getElementById("p_sned_retern").classList.remove("d-none");
-                document.getElementById("p_sned_retern").classList.add("d-inline,text-danger");
+                document.getElementById("p_sned_retern").classList.add("d-inline");
                 document.getElementById("p_sned_retern").style.cursor = "pointer";
                 document.getElementById("wait_countdown_timer").classList.add("d-none");
             }
         };
         tick();
-    }
+    } // you can checnge here
     countdown(1, 45);
 
 }
-
+// if you click to ارسال مجدد کد 
 function sned_retern_sms() {
     send_ponenumber();
     document.getElementById("btn-hide").click();
@@ -63,6 +65,7 @@ function sned_retern_sms() {
     countdown_timer();
 }
 
+// if click to button ورود i go you to without page
 const send_code = () => {
     $.ajax({
         type: 'POST',
@@ -83,19 +86,15 @@ const send_code = () => {
     });
 }
 
+// it is for check input phone number to we import corrent kind number
 
-
-const check_input = () => {
+function check_input_send_phonenumber() {
     let input_phonenumber = $('#validation-phonenumber').val();
-
-    // console.log(input_phonenumber.charAt(0));
 
     if (input_phonenumber.length == 11 && input_phonenumber.charAt(0) == 0 && input_phonenumber.charAt(1) == 9) {
         document.getElementById("btnSend").classList.remove("btn-secondary");
         document.getElementById("btnSend").classList.add("btn-success");
         document.getElementById("btnSend").removeAttribute("disabled");
-
-
     } else if (input_phonenumber.length == 10 && input_phonenumber.charAt(0) == 9) {
         document.getElementById("btnSend").classList.remove("btn-secondary");
         document.getElementById("btnSend").classList.add("btn-success");
@@ -114,10 +113,23 @@ const check_input = () => {
         document.getElementById("btnSend").classList.add("btn-secondary");
 
     }
-    // else if()
-
-
 }
+
+
+function check_input_send_code() {
+    let input_code = $('#import_code').val();
+    if (input_code.length == 5) {
+        document.getElementById("btn_send_code").classList.remove("btn-secondary");
+        document.getElementById("btn_send_code").classList.add("btn-success");
+        document.getElementById("btn_send_code").removeAttribute("disabled");
+    }
+}
+
+//btn send
 document.getElementById("btnSend").setAttribute("disabled", "0");
 document.getElementById("btnSend").classList.remove("btn-success");
 document.getElementById("btnSend").classList.add("btn-secondary");
+// btn import code 
+document.getElementById("btn_send_code").setAttribute("disabled", "0");
+document.getElementById("btn_send_code").classList.remove("btn-success");
+document.getElementById("btn_send_code").classList.add("btn-secondary");
